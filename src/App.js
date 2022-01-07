01b1de37b2b1b5e54c3de2819e47a6f20347f28a
czj306 <xx1czj@gmail.com>
2022-01-07 17:39:12 +0800

    UPDATE：轮播图的实现

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import './App.css';
+import Swiper from 'react-tiga-swiper';
+import 'react-tiga-swiper/dist/index.css';
+
+function App() {  
+  const urls = [
+    "https://console.huaweicloud.com/dlv/vision/share/?id=a3b2adc379704410905310a5d089240f&locale=zh-cn&region=cn-east-3",
+    "https://console.huaweicloud.com/dlv/vision/share/?id=19458e36e0654a859cf9302dde08dc99&locale=zh-cn&region=cn-east-3"
+  ];
+
+  const styles = { flex: 1, backgroundColor: 'black', width: '100vw', }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Swiper
+      duration={200}
+      autoPlay={10000}
+      selectedIndex={0}
+      showIndicators={false}
+      indicator={null}
+      showDots={false}
+      dots={null}
+      // direction="vertical"
+      loop={true}
+    >
+      {
+        urls.map((item, key) => (
+          <iframe frameBorder={0} key={key} style={styles} src={item}></iframe>
+        ))
+      }
+    </Swiper>
   );
 }
 
